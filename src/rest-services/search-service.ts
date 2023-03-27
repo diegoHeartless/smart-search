c091f67adbcd457dc19870c3c42f674792f43ea8
diegoHeartless <mnnvh@yandex.ru>
2023-03-28 01:05:26 +0300

    add proxy

--- src/rest-services/search-service.ts
+++ src/rest-services/search-service.ts
@@ -13,9 +13,9 @@ export const ozonSearch = async (search: string, redirectUrl?: string, page?: nu
     if (redirectUrl) {
         response = await axios.get(redirectUrl + tf_state ? `&tf_state=${tf_state}`: '')
     } else {
-        console.log(`https://www.ozon.ru/search/?text=${search}&from_global=true` + (page ? `&page=${page}`: '') + (tf_state ? `&tf_state=${tf_state}`: ''))
+        console.log(`/ozon/search/?text=${search}&from_global=true` + (page ? `&page=${page}`: '') + (tf_state ? `&tf_state=${tf_state}`: ''))
         response = await axios.get(
-            `https://www.ozon.ru/search/?text=${search}&from_global=true` + (page ? `&page=${page}`: '') + (tf_state ? `&tf_state=${tf_state}`: ''));
+            `/ozon/search/?text=${search}&from_global=true` + (page ? `&page=${page}`: '') + (tf_state ? `&tf_state=${tf_state}`: ''));
 
     }
     console.log(response)
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    console.log('test')
     app.use(
-        '/api',
+        '/ozon',
         createProxyMiddleware({
-            target: 'http://localhost:3080',
+            pathRewrite: function (path) {
+                console.log(path)
+                return path.replace('/ozon', '/')
+            },
+            onProxyRes: function (proxyReq, req, rsp) {
+                 proxyReq.headers['origin'] = 'http://mnnvh.fvds.ru';
+                   proxyReq.headers['referer'] = 'http://mnnvh.fvds.ru/';
+            },
+            target: 'http://www.ozon.ru',
             changeOrigin: true,
         })
     );
